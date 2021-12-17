76cee84f2ccd21f274fc2ffee3cf42f9d306dd94
VinhTruong2001 <ducvinh.truong2001@gmail.com>
2021-12-17 09:37:47 +0700

    Change twitter & telegram link

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -112,7 +112,7 @@
                 </a>
               </li>
               <li>
-                <a href="https://twitter.com/elasticbitcoin">
+                <a href="https://twitter.com/xbladecryptowar">
                   <i class="fab fa-twitter"></i>
                 </a>
               </li>
@@ -122,7 +122,7 @@
                 </a>
               </li>
               <li>
-                <a href="https://t.me/elasticbitcoinxbt">
+                <a href="https://t.me/cryptowarxblade">
                   <i class="fab fa-telegram"></i>
                 </a>
               </li>
